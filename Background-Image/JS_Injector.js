d5a29ea30f589627e1d598e8f96edc16305b55e4
pimenta1807 <arthurpimenta18@gmail.com>
2023-11-08 21:52:46 -0300

    Relase

---
+++ Background-Image/JS_Injector.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", async function () {
+    let s = setInterval(async function () {
+      const A = document.getElementsByClassName("flex-grow mt-12 md:mt-0 pl-0 md:pl-64")
+      const B = document.getElementsByClassName("w-full 2xl:even:pl-3 2xl:odd:pr-3")
+      if (A.length > 0) {
+        // adding the class for styling the image
+        A[0].classList.add('bg-image'); 
+      }
+      for (let i = 0; i < B.length; i++) {
+         // adding a class for every server on the servers list, so we can add a background color to it
+         // otherwise the trasnperency of the game image will show the bg-image 
+        B[i].classList.add('server-page-fix' );
+      }
+    }, 100); // works well for me
+  });
